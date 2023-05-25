89e363269e1381712eebef2ec6212699802b7648
pablogimenez <pablo.gimenez.zorrilla@gmail.com>
2023-05-25 07:13:28 -0400

    build ✈ inicial commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './store'
+import { TodoApp } from './TodoApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={ store }>
+      {/* <PokemonApp /> */}
+      <TodoApp />
+    </Provider>
+  </React.StrictMode>,
+)
